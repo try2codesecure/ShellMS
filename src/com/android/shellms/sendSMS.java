ce42f7833b1c421a965351f5a680d21dc3096069
kiz56fvdw2e <is101024@fhstp.ac.at>
2013-04-24 11:15:20 +0200

    new username, move project

--- src/com/android/shellms/sendSMS.java
+++ src/com/android/shellms/sendSMS.java
@@ -1,5 +1,6 @@
 /*	Copyleft 2013, by Rainer is101024@fhstp.ac.at
  *	University of Applied Sciences St.Pölten - http://www.fhstp.ac.at
+ *	Project Home = https://github.com/try2codesecure/ShellMS
  *	This file is part of ShellMS (GPLv3 - https://www.gnu.org/licenses/gpl-3.0.html)
  *  ShellMS is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
  *  ShellMS is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
